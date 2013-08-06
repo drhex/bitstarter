f2c90b0550dd83626bc08bd80bca3e1b6afca35e
Ubuntu <ubuntu@ip-172-31-41-247.us-west-2.compute.internal>
2013-08-06 05:02:40 +0000

    all write encoding, ascii read encoding

--- web.js
+++ web.js
@@ -6,17 +6,15 @@ app.use(express.logger());
 var contentsOfFile = new Buffer(256);
 
 
-fs.readFileSync('./index.html', function(err, contentsOfFile) {
+fs.readFileSync('./index.html', 'ascii' function(err, contentsOfFile) {
   if (err) throw err;
 //  console.log(contentsOfFile);
 });
 
-//  console.log(fs.readFileSync('./index.html', null));
 
 
 app.get('/', function(request, response) {
   response.send('test 1 \n' + contentsOfFile + '\n test 2 \n' + contentsOfFile.toString('ascii') + '\n test 3 \n' + contentsOfFile.toString("utf-8") );
-  response.send('test 1 \n' + contentsOfFile + '\n test 2 \n' + contentsOfFile.toString('ascii')  );
 });
 
 

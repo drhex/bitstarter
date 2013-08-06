0ba35491370008a842ac6d0ebf6ac3a80e904b29
Ubuntu <ubuntu@ip-172-31-41-247.us-west-2.compute.internal>
2013-08-06 04:20:56 +0000

    trying ascii for both read and tostring

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ app.use(express.logger());
 
 var contentsOfFile = new Buffer(256);
 
-fs.readFile('./index.html', 'utf-8', function(err, contentsOfFile) {
+fs.readFile('./index.html', 'ascii', function(err, contentsOfFile) {
   if (err) throw err;
 //  console.log(contentsOfFile.toString());
 });
 
 app.get('/', function(request, response) {
-  response.send(contentsOfFile.toString('utf-8'));
+  response.send(contentsOfFile.toString('ascii'));
 });
 
 
